3c59510ed388d10e36bd35b7888e899774a06ffb
Gabriel Adrian <gabriel.correa2@alunos.sc.senac.br>
2021-06-24 15:39:05 -0300

    Alterações

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,38 @@
-var exibirSobre = function(){
+
+var modoNoturno = function () {
+    var body = document.getElementById("corpo");
+    let fundo_cor = document.querySelector(".fundo_cor");
+    let borda = document.querySelector(".borda");
+    let titulo = document.querySelectorAll(".titulo");
+
+
+    if (body.classList.contains("modo-noturno")) {
+
+      body.classList.remove("modo-noturno");
+      fundo_cor.classList.remove("modo-noturno");
+      borda.classList.remove("modo-noturno");
+      titulo.forEach(
+          (element)=>{
+               element.classList.remove("modo-noturno");
+          }
+      );
+     
+
+    } else {
+
+      body.classList.add("modo-noturno");
+      fundo_cor.classList.add("modo-noturno");
+      borda.classList.add("modo-noturno");
+      titulo.forEach(
+        (element)=>{
+            element.classList.add("modo-noturno");
+        }
+    );
+     
+    }
+  }
+  
+  var exibirSobre = function(){
     divSobre = document.querySelector('#sobre');
     divHab = document.querySelector('#habiliade');
     divForm = document.querySelector('#formacao');
@@ -51,6 +85,7 @@ var exibirHob = function(){
     divBok.style.display = "none";
 }
 var exibirBok = function(){
+    console.log('clicado no exibir book');
     divSobre = document.querySelector('#sobre');
     divHab = document.querySelector('#habiliade');
     divForm = document.querySelector('#formacao');
